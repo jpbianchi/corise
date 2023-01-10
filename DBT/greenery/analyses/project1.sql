d5f6a290698775bda6f02ecaee434df06b4d9ebe
Jean-Pierre Bianchi <jpbianchi@gmail.com>
2023-01-10 14:32:38 +0000

    sql code update

--- DBT/greenery/analyses/project1.sql
+++ DBT/greenery/analyses/project1.sql
@@ -1,38 +1,80 @@
 
-
 with
-orders as (
-    select 
-        address_id
-        ,count(distinct order_id) over (partition by address_id) as orders_cnts
-        ,count(distinct order_id) over (partition by extract(hour from created_at)) as orders_hour  -- it is wrong to mix with address_id 
-        ,datediff('min', created_at, delivered_at) as delivery_time
 
-    from {{ source('postgres', 'orders')}}
+orders_counts as (
+    select 
+        count(user_id) as nb_users
+        ,sum((orders_cnts = 1)::int) as cnts1
+        ,sum((orders_cnts = 2)::int) as cnts2
+        ,sum((orders_cnts = 3)::int) as cnts3
+        ,sum((orders_cnts > 0)::int) as total_orders
+
+    from (
+        select 
+            address_id
+            ,user_id
+            ,count(distinct order_id) as orders_cnts
+
+        from {{ source('postgres', 'orders') }}
+        -- where address_id = '02331e89-1736-4f12-85b9-ddd62545214b'  -- test
+        group by address_id
+    )
 )
 
-,sessions as (
+,delivery_times as (
     select 
-        extract(hour from created_at) as hour
-        ,count(distinct session_id) over (partition by hour) as avg_sessions_hour
-    from {{ source('postgres', 'events')}}    
+        (avg(datediff('min', created_at, delivered_at)) / 60)::decimal(5,2) as avg_delivery_time_hr 
+
+    from {{ source('postgres', 'orders') }}
+    where STATUS = 'delivered'  -- some delivery times are missing
+)
+
+
+,orders_hr as (
+    select 
+        avg(orders_hr) as avg_orders_hr  -- 7.52
+
+    from 
+        (select 
+            count(distinct order_id) as orders_hr
+        from {{ source('postgres', 'orders') }}
+        group by date_trunc('hour', created_at) )    -- removes mins and seconds
+)
+
+,orders_hr2 as (
+    select 
+        avg(orders_hr) as avg_orders_hr2  -- 8.62
+
+    from 
+        (select 
+            count(distinct order_id) over (partition by date_trunc('hour', created_at)) as orders_hr
+        from {{ source('postgres', 'orders') }})
+)
+
+
+,sessions_hr as (
+    select
+        avg(sessions_hour) as avg_sessions_hr
+
+    from (
+        select 
+            count(distinct session_id) as sessions_hour
+
+        from {{ source('postgres', 'events') }}   
+        group by date_trunc('hour', created_at)
+    )
 )
 
 select 
-    address_id
-    ,orders_cnts
-    ,orders_hour
-    ,delivery_time
-from orders
-order by address_id
+    nb_users                -- 136
+    ,cnts1                  -- 24
+    ,cnts2                  -- 46
+    ,cnts3                  -- 37
+    ,total_orders           -- 136
+    ,avg_orders_hr          -- 7.52
+    ,avg_orders_hr2         -- 8.62  <<< WHY is it different ? 
+    ,avg_delivery_time_hr   -- 93.4
+    ,avg_sessions_hr        -- 16.33
+    
+from orders_counts, delivery_times, orders_hr, orders_hr2, sessions_hr
 
--- select
---     count(distinct address_id) as nb_users -- 136
---     ,sum(case when orders_cnts = 1 then 1 else 0 end) as orders_cnt1  -- 24  should be 24
---     ,sum(case when orders_cnts = 2 then 1 else 0 end) as orders_cnt2  -- 92  should be 46
---     ,sum(case when orders_cnts = 3 then 1 else 0 end) as orders_cnt3  -- 111  should be 37 - it's like the answer is multiplied by the cnt 3x37 = 111
---     ,count(orders_cnts) as orders_total                               -- 361 should be 136 
---     ,avg(orders_hour)::decimal(10,2) as avg_orders_hr                 -- 16.21
---     ,avg(delivery_time / 60)::decimal(10,2) as avg_delivery_time_hrs  -- 93.4 
-
--- from orders
